9c6a9bb333adb7622563a1f287fcca928762c43e
moreirathomas <moreirathomas97@gmail.com>
2021-06-17 23:47:08 +0200

    Refactor handler functions

--- cmd/static/main.go
+++ cmd/static/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/GregoryAlbouy/shrinker/pkg/dotenv"
 )
@@ -25,8 +26,10 @@ func main() {
 	envPath := dotenv.GetPath(defaultEnvPath)
 	dotenv.Load(envPath, &env)
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./storage"))))
-	http.HandleFunc("/static/avatar", handleFileUpload)
+	// GET /static/<filename>
+	http.Handle("/static/", handleFileServe("/static", env["STATIC_FILE_PATH"]))
+	// POST /static/avatar
+	http.HandleFunc("/static/avatar", handleImageUpload)
 
 	addr := ":" + env["STATIC_SERVER_PORT"]
 	fmt.Printf("Server listening at http://localhost%s\n", addr)
@@ -36,7 +39,14 @@ func main() {
 	}
 }
 
-func handleFileUpload(w http.ResponseWriter, r *http.Request) {
+// handleFileServe serves files under the given directory for the given path.
+func handleFileServe(path string, dir string) http.Handler {
+	return http.StripPrefix(path, disableDirListing(http.FileServer(http.Dir(dir))))
+}
+
+// handleImageUpload handles requests to upload an image to the server.
+// The uploaded image is save on the disk if the request is accepted.
+func handleImageUpload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "bad request", 400)
 		return
@@ -64,4 +74,18 @@ func handleFileUpload(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(201)
 	w.Write([]byte("Created\n"))
+}
+
+// disableDirListing prevents http.FileServer from automatically generating
+// navigable directory listings.
+// It simply handles every path ending with a trailing slash as a 404.
+func disableDirListing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.Error(w, "not found", 404)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
