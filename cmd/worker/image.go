5831cb5e6858d0bb59e10c5992680ac47d5c3065
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-06-20 21:21:35 +0200

    Create processImage helper

---
+++ cmd/worker/image.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/GregoryAlbouy/shrinker/pkg/imaging"
+)
+
+const (
+	resizeWidth = 200               // TODO: get this value via CLI flag
+	imageFormat = imaging.PNGFormat // TODO: make it actionnable
+)
+
+// processImage executes the whole image processing logic.
+// It takes a slice of bytes in input, decodes it to an image,
+// performs the rescale, and finally return a reader to access
+// the resulting file along with the image file extension.
+// It returns a non-nil error if anything went wrong.
+//
+// FIXME: imaging.DecodeRaw produces an error
+func processImage(raw []byte) (io.Reader, imaging.Ext, error) {
+	// // temp debug
+	// log.Println("Processing image")
+
+	// Decode raw file
+	img, err := imaging.DecodeRaw(raw)
+	if err != nil {
+		return nil, "", fmt.Errorf("image decoding error: %w", err)
+	}
+
+	// Rescale
+	resized := imaging.Rescale(img, resizeWidth)
+
+	// Encode and retrieve a file reader
+	reader, err := imaging.Reader(resized, imageFormat)
+	if err != nil {
+		return nil, "", fmt.Errorf("image encoding error: %w", err)
+	}
+
+	// Get file extension
+	ext := imaging.FormatExt[imageFormat]
+
+	return reader, ext, nil
+}
