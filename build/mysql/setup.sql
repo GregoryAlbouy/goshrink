4dd0f6f5955fd079eba7aff0861142f0a46253f8
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-06-22 15:40:23 +0200

    Add NOT NULL constraint to user columns

--- build/mysql/setup.sql
+++ build/mysql/setup.sql
@@ -4,9 +4,9 @@ USE goshrink;
 -- user table
 CREATE TABLE IF NOT EXISTS user (
 	id INT UNSIGNED AUTO_INCREMENT,
-	username VARCHAR(20) UNIQUE,
-	password VARCHAR(255),
-	email VARCHAR(255) UNIQUE,
+	username VARCHAR(20) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
 	PRIMARY KEY (id)
 );
 
